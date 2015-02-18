a3e69b46e871908093bcf34db5529eea47c8e5cf
Alexander Hosking <alexander@ahosking.com>
2015-02-17 20:38:41 -0500

    DB trials and workings

---
+++ db_learning.py
@@ -0,0 +1,30 @@
+#Database testing and learning
+
+import sqlite3 as lite
+import sys
+
+con = None
+
+con = lite.connect('new.db')
+
+with con:
+    cur = con.cursor()
+
+    #Create the table for seasons
+    try:
+        cur.execute("CREATE TABLE Seasons(Id INT, Year_start INT, Season TEXT)")
+    except:
+        pass
+
+    #Insert a season!
+    cur.execute("INSERT INTO Seasons VALUES(1, 2014, 'Winter')")
+
+    #Create the Games Table
+    try:
+        cur.execute("CREATE TABLE Games(Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Time INT, Rink TEXT, Home_Team TEXT, Home_Score INT, Away_Team TEXT, Away_Score INT)")
+    except:
+        pass
+
+    #Add a game
+    cur.execute("INSERT INTO Games VALUES(rowid,'Sep 14', 21.00, 'Rinx 1', 'Toronto Ice Dogs', 4, 'Shock', 6)")
+    cur.execute("INSERT INTO Games VALUES(rowid,'Sep 21', 17.15, 'Rinx 2', 'Lucky Pucks', 6, 'Toronto Ice Dogs', 6)")
