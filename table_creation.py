cb5ee1a1480d0e78f490a6ee5eb6deb201f7690d
Alexander Hosking <alexander@ahosking.com>
2015-02-19 00:53:34 -0500

    Created the gamesheet table and have started the work to parse it.

--- table_creation.py
+++ table_creation.py
@@ -17,11 +17,30 @@ with con:
         pass
 
     #Insert a season!
-    cur.execute("INSERT INTO Seasons VALUES(1, 2014, 'Winter')")
+    # cur.execute("INSERT INTO Seasons VALUES(1, 2014, 'Winter')")
 
     #Create the Games Table
     try:
         cur.execute("CREATE TABLE Games(Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Time TEXT, Rink TEXT,\
-         Home_Team TEXT, Home_Score INT, Away_Team TEXT, Away_Score INT, Season TEXT)")
+         Home_Team TEXT, Home_Score INT, Away_Team TEXT, Away_Score INT, Season TEXT, game_sheet TEXT)")
     except:
         pass
+
+    #Create the Players Table
+    try:
+        # print "Player\t\t\t\tGP\t G\t A\t Pts\t PIM"
+        cur.execute("CREATE TABLE Players(Id INTEGER PRIMARY KEY AUTOINCREMENT,player_number TEXT, player_name TEXT,\
+         games_played INT, goals INT, assists INT, points INT, penalty_minutes INT, Season TEXT)")
+        print "Player table created!"
+    except:
+        print "Player table creation failed."
+
+def createGameSheets():
+    # Game Index Number Per	Time	Player#	PIM	Penalty   Season
+    cur.execute("CREATE TABLE gameSheets(GameIndex INT, roster_number TEXT, roster_name TEXT, roster_goals INT,\
+     roster_assists INT, roster_points INT, roster_pim INT, goals_period INT, goals_time TEXT, goals_assist1 TEXT,\
+     goals_assist2 TEXT, penalty_period INT, penalty_time TEXT, penalty_player INT, penalty_pim INT,\
+     penalty_call TEXT)")
+    print "Game sheet creation succeeded?!"
+
+createGameSheets()
--- teamIndex.py
+++ teamIndex.py
@@ -12,11 +12,24 @@ def add_games(new_games):
     with con:
         cur = con.cursor()
         for i in new_games:
-            cur.execute("INSERT INTO Games VALUES(null, ?, ?, ?, ?, ?, ?, ?, ?)",\
+            cur.execute("INSERT INTO Games VALUES(null, ?, ?, ?, ?, ?, ?, ?, ?, ?)",\
+                        (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8]))
+            # cur.execute("INSERT INTO Games VALUES(null, 'Sep 14', '21:00', 'Rinx 1', 'Toronto Ice Dogs', 4, 'Shock', 6, "Winter2014", "Gamesheet")")
+
+        print "\nSuccessfully added games!\n"
+
+def add_players(new_players):
+    #Make DB Connection
+    con = lite.connect('new.db')
+
+    with con:
+        cur = con.cursor()
+        for i in new_players:
+            print i
+            cur.execute("INSERT INTO Players VALUES(null, ?, ?, ?, ?, ?, ?, ?, ?)",\
                         (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7]))
-            # cur.execute("INSERT INTO Games VALUES(null, 'Sep 14', '21:00', 'Rinx 1', 'Toronto Ice Dogs', 4, 'Shock', 6)")
-
-
+            # "Player    GP    G    A    Pts    PIM   Season"
+        print "\nSuccessfully added players.\n"
 
 soup = BeautifulSoup(urllib2.urlopen("http://www.truenorthhockey.com/asp_pages/Team.aspx?team_id=17178").read())
 
@@ -26,27 +39,48 @@ soup = BeautifulSoup(urllib2.urlopen("http://www.truenorthhockey.com/asp_pages/T
 # print soup.find_all('table')[8]
 # print soup.find_all('table')[8].findAll('tr')[1]
 playerCount = range(1,len(soup.find_all('table')[8]) - 2)
-
+players = []
 #This will collect all player data for the current season
-print "Player\t\t\t\tGP\t G\t A\t Pts\t PIM"
+print "Number\tPlayer\t\t\t\tGP\t G\t A\t Pts\t PIM"
 for i in playerCount:
-    print soup.find_all('table')[8].findAll('tr')[i].findAll('td')[1].string,"\t\t\t",\
-        soup.find_all('table')[8].findAll('tr')[i].findAll('td')[2].string,"\t",\
-        soup.find_all('table')[8].findAll('tr')[i].findAll('td')[3].string,"\t",\
-        soup.find_all('table')[8].findAll('tr')[i].findAll('td')[4].string,"\t",\
-        soup.find_all('table')[8].findAll('tr')[i].findAll('td')[5].string,"\t",\
-        soup.find_all('table')[8].findAll('tr')[i].findAll('td')[6].string
-print
+    player = []
+    player.append(soup.find_all('table')[8].findAll('tr')[i].findAll('td')[0].string)
+    player.append(soup.find_all('table')[8].findAll('tr')[i].findAll('td')[1].string)
+    player.append(soup.find_all('table')[8].findAll('tr')[i].findAll('td')[2].string)
+    player.append(soup.find_all('table')[8].findAll('tr')[i].findAll('td')[3].string)
+    player.append(soup.find_all('table')[8].findAll('tr')[i].findAll('td')[4].string)
+    player.append(soup.find_all('table')[8].findAll('tr')[i].findAll('td')[5].string)
+    player.append(soup.find_all('table')[8].findAll('tr')[i].findAll('td')[6].string)
+    player.append("Winter2014")
+    players.append(player)
+    # print soup.find_all('table')[8].findAll('tr')[i].findAll('td')[1].string,"\t\t\t",\
+    #     soup.find_all('table')[8].findAll('tr')[i].findAll('td')[2].string,"\t",\
+    #     soup.find_all('table')[8].findAll('tr')[i].findAll('td')[3].string,"\t",\
+    #     soup.find_all('table')[8].findAll('tr')[i].findAll('td')[4].string,"\t",\
+    #     soup.find_all('table')[8].findAll('tr')[i].findAll('td')[5].string,"\t",\
+    #     soup.find_all('table')[8].findAll('tr')[i].findAll('td')[6].string
+#Add the players!
+add_players(players)
+
 
 #Table 9 seems to carry all goalie data
 goalieCount = range(1,len(soup.find_all('table')[9]) - 2)
+goalies = []
 print "Player\tGP\tGA\tGAA\tShO"
 for i in goalieCount:
-    print soup.find_all('table')[9].findAll('tr')[i].findAll('td')[1].string,"\t",\
-    soup.find_all('table')[9].findAll('tr')[i].findAll('td')[2].string,"\t",\
-    soup.find_all('table')[9].findAll('tr')[i].findAll('td')[3].string,"\t",\
-    soup.find_all('table')[9].findAll('tr')[i].findAll('td')[4].string,"\t",\
-    soup.find_all('table')[9].findAll('tr')[i].findAll('td')[5].string
+    goalie = []
+    goalie.append(soup.find_all('table')[9].findAll('tr')[i].findAll('td')[0].string)
+    goalie.append(soup.find_all('table')[9].findAll('tr')[i].findAll('td')[1].string)
+    goalie.append(soup.find_all('table')[9].findAll('tr')[i].findAll('td')[2].string)
+    goalie.append(soup.find_all('table')[9].findAll('tr')[i].findAll('td')[3].string)
+    goalie.append(soup.find_all('table')[9].findAll('tr')[i].findAll('td')[4].string)
+    goalie.append("Winter2014")
+    # print soup.find_all('table')[9].findAll('tr')[i].findAll('td')[1].string,"\t",\
+    # soup.find_all('table')[9].findAll('tr')[i].findAll('td')[2].string,"\t",\
+    # soup.find_all('table')[9].findAll('tr')[i].findAll('td')[3].string,"\t",\
+    # soup.find_all('table')[9].findAll('tr')[i].findAll('td')[4].string,"\t",\
+    # soup.find_all('table')[9].findAll('tr')[i].findAll('td')[5].string
+    print goalie
 print
 
 
@@ -64,7 +98,12 @@ for i in schedCount:
     game.append(soup.find_all('table')[10].findAll('tr')[i].findAll('td')[5].string)
     game.append(soup.find_all('table')[10].findAll('tr')[i].findAll('td')[6].string)
     game.append("Winter2014")
+    #Get the score sheets
+    game.append((soup.find_all('table')[10].findAll('tr')[i].findAll('a')[1]).get('href'))
+
     games.append(game)
+    #add game sheet URL
+    # gameSheets.append("http://www.truenorthhockey.com/asp_pages/" + (soup.find_all('table')[10].findAll('tr')[i].findAll('a')[1]).get('href'))
     # print soup.find_all('table')[10].findAll('tr')[i].findAll('td')[0].string,\
     #     soup.find_all('table')[10].findAll('tr')[i].findAll('td')[1].string,\
     #     soup.find_all('table')[10].findAll('tr')[i].findAll('td')[2].string,\
@@ -74,7 +113,18 @@ for i in schedCount:
     #     soup.find_all('table')[10].findAll('tr')[i].findAll('td')[6].string
 add_games(games)
 
+print "\n-------------------------------------------------------------------\n"
+print "Game Sheet\n"
+testSheet = ("http://www.truenorthhockey.com/asp_pages/" + games[1][8])
+print testSheet
+gameSoup = BeautifulSoup(urllib2.urlopen(testSheet).read())
 
+print gameSoup.prettify()
 
 #We need to find links to the score sheets
-print (soup.find_all('table')[10].findAll('tr')[6].findAll('a')[1]).get('href')
+# print (soup.find_all('table')[10].findAll('tr')[6].findAll('a')[1]).get('href')
+
+##################################
+##Game Sheets
+##################################
+# Game Index Number Per	Time	Player#	PIM	Penalty   Season
